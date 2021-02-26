0ad4e382f9193a2e952d37c0f07552ab482b00aa
JyLIU-emma <bfsufyxljy@163.com>
2021-02-26 17:22:47 +0100

    traitement import de json

--- backend/resources/admins.py
+++ backend/resources/admins.py
@@ -11,32 +11,7 @@ __all__ =['Login', 'CreateAdmin']
 false = False
 true = True
 
-users = {
-    "001": {
-        "id": "001",
-        "nom": "Mikolov",
-        "prenom": "Thomas",
-        "fonction": "Directeur des representations vectorielles",
-        "anciennete": 10,
-        "mise_a_jour": "2017-05-06 11:25:11.827000",
-        "conge": 15,
-        "actif": false,
-        "actionnaire": true,
-        "missions": ["Bruxelle", "Paris", "Pakistan"]
-    },
-    "002": {
-        "id": "002",
-        "nom": "Bergier",
-        "prenom": "Francine",
-        "fonction": "Chef service matériel informatique",
-        "anciennete": 20,
-        "mise_a_jour": "2011-01-05 06:20:31.527000",
-        "conge": 14,
-        "actif": true,
-        "actionnaire": false,
-        "missions": ["Paris"]
-    }
-    }
+users = load_data('users')
 
 class Login(Resource):
     def get(self, userid):
@@ -62,6 +37,7 @@ class Login(Resource):
 
 
         #second：
+        
         username = request.form.get('username')
         password = request.form.get('password')
 
@@ -69,7 +45,8 @@ class Login(Resource):
             if username == users[user]['nom']:
                 msg = f"welcome, {username}"
                 return {'message': msg, 'userinfo':users[user]}
-            return "Hello stranger"
+
+        return "Hello stranger"
 
 
 
--- backend/resources/lib/utils.py
+++ backend/resources/lib/utils.py
@@ -1,5 +1,20 @@
 
-__all__ = ["test1", "test1_b"]
+from pathlib import Path
+import json
+
+__all__ = ["test1", "test1_b", "load_data"]
+
+# Créer des chemins vers les bases de données
+BACKEND = Path(__file__).parent.parent.parent
+DATA = BACKEND / "data"
+USERS_DATA = DATA / "users.json"
+
+def load_data(tablename):
+    if tablename == "users":
+        with USERS_DATA.open() as f:
+            data_dico = json.load(f) 
+    return data_dico
+    
 
 def test1():
     print("Print all the users in the browser")
@@ -8,4 +23,10 @@ def test1_b():
     print("this is test1_b in utils.py")
 
 
+def main():
+    users_dico = load_data("users")
+    for key in users_dico:
+        print(key)
 
+if __name__ == "__main__":
+    main()
