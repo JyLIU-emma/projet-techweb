134efc4769ccb6a3da9ce8fd20bc547d8c58c362
xinyi1214 <60469962+xinyi1214@users.noreply.github.com>
2021-02-28 22:44:45 +0100

    Update admins.py

--- backend/resources/admins.py
+++ backend/resources/admins.py
@@ -1,65 +1,57 @@
-from flask import Flask, request, flash, url_for, redirect, render_template, session, flash, jsonify
+from flask import Flask, request, url_for, redirect, session, flash, jsonify, abort
 import json
+# from flask_login import LoginManager, UserMixin, login_required, logout_user
+from flask_login import login_user, logout_user, login_required
 import os
 from flask_restful import Resource, Api
-
+from passlib.apps import custom_app_context as pwd_context
+import jwt
 from .lib.utils import *
 
 
-__all__ =['Login', 'CreateAdmin']
+__all__ =['Login', 'CreateAdmin', 'Logout']
 
 false = False
 true = True
 
+
 users = load_data('users')
-admins = load_data('admins')
+#admins = load_data('admins')
+
+
 
 class Login(Resource):
     def get(self, userid):
         return jsonify({userid:users[userid], 'nom': username, 'mot de passe': password})
     
     def post(self):
-        """
-        first:
-        http://127.0.0.1:5000/admins/login/002?field=fonction
-
-        {
-            "002": "Chef service matériel informatique"
-        }
-
-        second:
-        http://127.0.0.1:5000/admins/login
-        postman: body form-data 输入信息
-        """
-        #first:
-        # searchfield = request.args['field']  # searchfield 是我们想查询的字典里的键
-        # return {userid:users[userid][searchfield]}
-
-
-        #second：
-
         username = request.form.get('username')
+        userid = request.form.get('id')
         password = request.form.get('password')
 
-        session['name'] = username
-        session['password'] = password
+        if not all([username, password, userid]):
+            msg = "Remplissez tous les champs, s'il vous plaît!"
+            abort(400, msg)
 
-        # print(session['name'])
-        # print(session['password'])
+        #session['name'] = username
+        #session['id'] = userid
+        #session['password'] = password
 
-        if not all([username, password]):
-            msg = "Rempliez tous les champs, s'il vous plaît!"
-            return {'message' : msg}
+        user = User.query.filter_by(id = userid).first()
+        if not user or not user.verify_password(password):
+            msg = "Vérifiez votre nom, votre id ou votre mot de passe, s'il vous plaît !"
+            abort(400, msg)
+        #----增加----#
         else:
-            for user in users:
-                if username == users[user]['nom'] and password == users[user]['id']:
-                    msg = f"welcome, {username}"
-                    return jsonify({'message': msg, 'userinfo':users[user]})
-                elif username == users[user]['nom'] and password != users[user]['id']:
-                    msg = "Le mot de passe n'est pas correct"
-                    return jsonify({'message': msg})
-            return jsonify({'message' : "Hello stranger"})
+            login_user(user, remember = False)
+            return jsonify({"message" : "Bienvenu."})
 
+
+class Logout(Resource):
+    @login_required
+    def get(self):
+        logout_user()
+        return jsonify({'message':"Vous êtes sorti le système."})
 
 
 class CreateAdmin(Resource):
@@ -68,29 +60,31 @@ class CreateAdmin(Resource):
     
     def post(self):
         username = request.form.get('username')
+        #print(username)
         userid = request.form.get('id')
+        #print(userid)
         password = request.form.get('password')
         password2 = request.form.get('passwordconfirm')
 
-        if userid not in users.keys():
-            msg = "Désolée, vous n'êtes pas notre collaborateur, vous ne pouvez pas créer un compte."
-        elif userid in admins.keys():
-            msg = "Vous avez déjà un compte."
-        elif not all([username, userid, password, password2]):
-            msg = "Veuillez remplir tous les champs."
+        if not all([username, userid, password, password2]):
+            abort(400, "Veuillez remplir tous les champs.") #missing arguments
+        elif userid not in users.keys():
+            abort(400, "Désolée, vous n'êtes pas notre collaborateur, vous ne pouvez pas créer un compte.")
+        elif User.query.filter_by(username = username).first() is not None:
+            abort(400, 'Vous avez déjà un compte.') #existing user
         elif username != users[userid]["nom"] :
-            msg = "Votre id ne conforme pas à votre nom. "
+            abort(400, "Votre id ne conforme pas à votre nom. ")
         elif password != password2 :
-            msg = "Les deux mots de passe remplis doivent être identiques."
-        else:
-            admins[userid] = {
-                    'id': userid,
-                    'nom': username,
-                    'mot de passe': password
-                    }
-            msg = "Votre compte admin a bien été créé."
+            abort(400, "Les deux mots de passe remplis doivent être identiques.")
+
+        user = User(username = username, id = userid)
+        user.hash_password(password)
+        db.session.add(user)
+        db.session.commit()
+
+        msg = "Votre compte admin a bien été créé."
         
         # print(admins)
-        dict_to_json(admins, "admins")
+        # dict_to_json(admins, "admins")
 
         return jsonify({"massage" : msg})
