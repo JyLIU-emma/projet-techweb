07b1d3df4d97850745d61f04c311f6e364901790
xinyi1214 <60469962+xinyi1214@users.noreply.github.com>
2021-02-28 22:47:40 +0100

    Update api.py

--- backend/api.py
+++ backend/api.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, render_template, flash, session, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource, Api
-
+from flask_httpauth import HTTPBasicAuth
+from flask_login import LoginManager
 from pathlib import Path
 
 from resources import *
@@ -17,6 +18,31 @@ app.config['JSON_SORT_KEYS'] = False
 
 db.init_app(app)
 api = Api(app)
+auth = HTTPBasicAuth()
+#----增加----#
+login_manager = LoginManager()
+login_manager.init_app(app)
+@login_manager.user_loader
+def load_user(user_id):
+    """Loads the user. Required by the `login` extension."""
+    user_instance = User.query.filter_by(id=user_id).first()
+    if user_instance:
+        return user_instance
+    else:
+        return None
+
+@auth.verify_password
+def verify_password(username_or_token, password):
+    # first try to authenticate by token
+    user = User.verify_auth_token(username_or_token, current_app)
+    logger.info('username:{} password:{}'.format(username_or_token, password))
+    if not user:
+        # try to authenticate with username/password
+        user = User.query.filter_by(name = username_or_token).first()
+        if not user or not user.check_password(password):
+            return False
+    g.current_user = user
+    return True
 
 
 api.add_resource(Home, '/', '/home')
